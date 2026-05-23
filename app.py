8bfdadfa5455b06ad9785e0dbc645ba0fc3c45ee
Dumini <DuminiWickramaarachchi.WickramaarachchiAppuhamilage@edu.fh-kaernten.ac.at>
2026-05-23 10:42:33 +0200

    Updated app for Render deployment

--- app.py
+++ app.py
@@ -299,5 +299,9 @@ def logout():
     session.clear()
     return redirect(url_for("login"))
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
